b40d06240c0c43dd19629785b4a2744c7ce3f8af
Rajkumar Verma <rajkumarverma70790@gmail.com>
2023-11-26 19:19:08 +0530

    Create 12_Dijistra_Shortest_Path.cpp

---
+++ 12_Dijistra_Shortest_Path.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> dis(V);
+        
+        for(int i = 0; i<V; i++)
+        {
+            dis[i] = 1e9;
+        }
+        
+        dis[S] = 0;
+        pq.push({dis[S], S});
+        
+        while(!pq.empty())
+        {
+            int d = pq.top().first;
+            int u = pq.top().second;
+            
+            pq.pop();
+            
+            for(auto it: adj[u] )
+            {
+                int wt = it[1];
+                int v = it[0];
+                
+                if(dis[u] + wt < dis[v])
+                {
+                    dis[v] = dis[u] + wt;
+                    pq.push({dis[v], v});
+                }
+            }
+        }
+        
+        return dis;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
